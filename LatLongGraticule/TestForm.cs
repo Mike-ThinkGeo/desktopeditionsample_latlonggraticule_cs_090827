85c3ca7078c3ca6b23e03f0b250005f7f1b46a6d
sicnuthinkgeo <1270896784@qq.com>
2016-09-05 20:04:11 +0800

    first commit

---
+++ LatLongGraticule/TestForm.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Windows.Forms;
+using ThinkGeo.MapSuite.Core;
+using ThinkGeo.MapSuite.DesktopEdition;
+
+namespace LatLongGraticule
+{
+    public partial class TestForm : Form
+    {
+        public TestForm()
+        {
+            InitializeComponent();
+        }
+
+        private void TestForm_Load(object sender, EventArgs e)
+        {
+            // Set the full extent and the background color
+            winformsMap1.CurrentExtent = new RectangleShape(-120, 25, 20, -30);
+            winformsMap1.BackgroundOverlay.BackgroundBrush = new GeoSolidBrush(GeoColor.FromArgb(255, 198, 255, 255));
+
+            // Add the static layers to the MapEngine
+            ShapeFileFeatureLayer worldLayer = new ShapeFileFeatureLayer(@"..\..\Data\Countries02.shp", ShapeFileReadWriteMode.ReadOnly);
+            worldLayer.ZoomLevelSet.ZoomLevel01.DefaultAreaStyle = AreaStyles.County1;
+            worldLayer.ZoomLevelSet.ZoomLevel01.ApplyUntilZoomLevel = ApplyUntilZoomLevel.Level20;
+            // mapEngine.StaticLayers.Add("WorldLayer", worldLayer);
+
+            LayerOverlay overLayer = new LayerOverlay();
+            overLayer.Layers.Add(worldLayer);
+            winformsMap1.Overlays.Add("Overlay", overLayer);
+
+            //Add the GraticuleAdormentLayer to the Adornment Layers of the MapEngine
+            GraticuleAdornmentLayer graticuleAdornmentLayer = new GraticuleAdornmentLayer();
+            winformsMap1.AdornmentOverlay.Layers.Add("graticule", graticuleAdornmentLayer);
+
+            winformsMap1.Refresh();
+        }
+
+        //Displays the world coordinates for the mouse pointer in decimal degrees and Degrees Minutes Seconds form.
+        private void winformsMap1_MouseMove(object sender, MouseEventArgs e)
+        {
+            PointShape worldPointShape = ExtentHelper.ToWorldCoordinate(winformsMap1.CurrentExtent, new ScreenPointF(e.X, e.Y), winformsMap1.Width, winformsMap1.Height);
+            try
+            {
+                lblLongitudeDMS.Text = DecimalDegreesHelper.GetDegreesMinutesSecondsStringFromDecimalDegree(worldPointShape.X);
+                lblLatitudeDMS.Text = DecimalDegreesHelper.GetDegreesMinutesSecondsStringFromDecimalDegree(worldPointShape.Y);
+
+                lblLongitude.Text = System.Convert.ToString(worldPointShape.X);
+                lblLatitude.Text = System.Convert.ToString(worldPointShape.Y);
+            }
+            catch
+            {
+                lblLongitudeDMS.Text = "N/A";
+                lblLatitudeDMS.Text = "N/A";
+
+                lblLongitude.Text = "N/A";
+                lblLatitude.Text = "N/A";
+            }
+            finally { }
+        }
+     
+  
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+    
+    }
+}
